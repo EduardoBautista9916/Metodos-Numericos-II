db4a2e508f08aa663dacf18b97cedf23a743bea0
Luis Eduardo Bautista Gaona <42494269+EduardoBautista9916@users.noreply.github.com>
2019-05-22 22:31:39 -0500

    Create menus.cpp

---
+++ PAQUETE/menus.cpp
@@ -0,0 +1,98 @@
+void despedida(){
+    system("cls");
+    barra(100);sl(1);
+    barra(27);
+    printf("HASTA LUEGO, GRACIAS POR UTILIZAR EL PROGRAMA");
+    barra(28);sl(1);
+    barra(100);sl(1);
+    barra(2);espacios(29);
+    printf("UNIVERSIDAD NACIONAL AUT%cNOMA DE M%cXICO",oO,eE);
+    espacios(28);barra(2);sl(1);
+    barra(2);espacios(29);
+    printf("FACULTAD DE ESTUDIOS SUPERIORES ACATL%cN", aA);
+    espacios(28);barra(2);sl(1);
+    barra(2);espacios(38);
+    printf("M%cTODOS NUM%cRICOS II",eE,eE);
+    espacios(38);barra(2);sl(1);
+    barra(2);espacios(41);
+    printf("PAQUETE FINAL");
+    espacios(42);barra(2);sl(1);
+    barra(2);espacios(5);
+    printf("Miembros del equipo:");
+    espacios(71);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%c%clvarez Cortes Kevin Alejandro", 126, aA);
+    espacios(55);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%cBautista Gaona Luis Eduardo",126);
+    espacios(58);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%cFlores Hern%cndez Itzel",126, a);
+    espacios(63);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%cFlores Velasco Jos%c Juan",126,e);
+    espacios(61);barra(2);sl(1);
+    barra(100);printf("\n");
+    system("pause");
+    system("cls");
+}
+
+void bienvenida(){
+    system("cls");
+    barra(100);sl(1);
+    barra(45);
+    printf("BIENVENIDO");
+    barra(45);sl(1);
+    barra(100);sl(1);
+    barra(2);espacios(29);
+    printf("UNIVERSIDAD NACIONAL AUT%cNOMA DE M%cXICO",oO,eE);
+    espacios(28);barra(2);sl(1);
+    barra(2);espacios(29);
+    printf("FACULTAD DE ESTUDIOS SUPERIORES ACATL%cN", aA);
+    espacios(28);barra(2);sl(1);
+    barra(2);espacios(38);
+    printf("M%cTODOS NUM%cRICOS II",eE,eE);
+    espacios(38);barra(2);sl(1);
+    barra(2);espacios(41);
+    printf("PAQUETE FINAL");
+    espacios(42);barra(2);sl(1);
+    barra(2);espacios(5);
+    printf("Miembros del equipo:");
+    espacios(71);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%c%clvarez Cortes Kevin Alejandro", 126, aA);
+    espacios(55);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%cBautista Gaona Luis Eduardo",126);
+    espacios(58);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%cFlores Hern%cndez Itzel",126, a);
+    espacios(63);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("%cFlores Velasco Jos%c Juan",126,e);
+    espacios(61);barra(2);sl(1);
+    barra(100);printf("\n");
+    system("pause");
+    system("cls");
+}
+
+void menu(){
+
+    barra(100);sl(1);
+    barra(2);espacios(2);
+    printf("En este Paquete Final tendremos el conjunto de todos los programas realizados durante el");
+    espacios(6);barra(2);sl(1);
+    barra(2);espacios(2);
+    printf("curso, es decir:");
+    espacios(78);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("1.- Sistema de Ecuaciones no Lineales");
+    espacios(49);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("2.- Interpolaci%cn y Ajuste de Curvas",o);
+    espacios(50);barra(2);sl(1);
+    barra(2);espacios(10);
+    printf("3.- Diferenciaci%cn e Integraci%cn", o, o);
+    espacios(54);barra(2);sl(1);
+    barra(100);sl(1);
+}
